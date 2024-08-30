f6bad59e2c5b15f8fca53c833055a62e7db7c335
Noah Sandler <noahsandler@gmail.com>
2024-08-30 09:44:03 -0400

    move enums to enum.h

--- main.h
+++ main.h
@@ -5,21 +5,6 @@
 #include <string.h>
 #include <time.h>
 
-enum DNSOpcode {
-    QUERY=0, // standard query
-    IQUERY=1, // inverse query
-    STATUS=2 // server status request
-};
-
-enum HeaderResponseCode {
-    NO_ERROR=0,
-    FORMAT_ERROR=1,
-    SERVER_FAILURE=2,
-    NAME_ERROR=3,
-    NOT_SUPPORTED=4, // Server doesn't support the requested kind of query
-    REFUSED=5
-};
-
 struct DNSHeader {
     uint16_t id;
     unsigned int qr: 1; // query (0) or response (1)?
