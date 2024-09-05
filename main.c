7e3e5e0d3346edf08fbe49df36f0c8c58ca9945d
Noah Sandler <noahsandler@gmail.com>
2024-09-05 14:36:45 -0400

    checkpoint

--- main.c
+++ main.c
@@ -8,38 +8,84 @@ void validate_alloc(void *mem_ptr) {
 }
 
 void init_header(struct DNSHeader *header) {
-    header->id = (uint16_t)(rand() % 100); // random number b/w 0 and 99, inclusive
-    header->rd = 1;
-    header->qdcount = 1;
+    header->id = htons(22); //(uint16_t)(rand() % 100); // random number b/w 0 and 99, inclusive
+    header->flags = htons(0b0000000100000000);
+    header->qdcount = htons(1);
 }
 
-char *encode_hostname(char *hostname) {
+void encode_hostname(char *hostname, char *en_hostname_out) {
     // Make deep copy of hostname to use with strtok()
     const size_t len_hostname = strlen(hostname) + 1; // +1 for null byte
     char hostname_cpy[len_hostname];
     strncpy(hostname_cpy, hostname, len_hostname);
 
-    char *res = (char *)calloc(len_hostname + 2, sizeof(char)); // +2 for leading/trailing token counts
     char *token = strtok(hostname_cpy, ".");
     size_t len_token;
     int pos = 0;
+
     while (token) {
         len_token = strlen(token);
-        sprintf(&res[pos], "%d", (int)len_token);
-        strncpy(&res[pos + 1], token, len_token);
+        snprintf(&en_hostname_out[pos], sizeof(uint16_t), "%lu", len_token);
+        strncpy(&en_hostname_out[pos + 1], token, len_token + 1);
         pos += len_token + 1; // move pos to start of where next token will go
         token = strtok(NULL, ".");
     }
 
-    sprintf(&res[pos], "%d", 0);
-
-    return res;
+    sprintf(&en_hostname_out[pos], "%d", 0);
 }
 
-void init_question(struct DNSQuestion *question, char *hostname) {
-    question->qname = (uint8_t *)encode_hostname(hostname);
-    question->qtype = QT_ALL;
-    question->qclass = QC_IN;
+void init_question(struct DNSQuestion *question, char *hostname, size_t *len_encoded_hostname_out) {
+    *len_encoded_hostname_out = strlen(hostname) + 2; // +2 for leading/trailing token counts
+    question->qname = (uint8_t *)calloc(*len_encoded_hostname_out, sizeof(uint8_t));
+    validate_alloc(question->qname);
+
+    encode_hostname(hostname, (char *)question->qname);
+
+    question->qtype = htons(QT_A);
+    question->qclass = htons(QC_IN);
+}
+
+uint8_t *pack_payload(struct DNS_msg *msg, char *hostname) {
+    size_t len_encoded_hostname;
+    init_header(msg->header);
+    init_question(msg->question, hostname, &len_encoded_hostname);
+
+    printf("hostname: %s\nencoded: %s\n", hostname, msg->question->qname);
+
+    const size_t payload_size = sizeof(msg->header) + (sizeof(uint16_t) * 2) + len_encoded_hostname; // -1 for question.qname pointer
+    uint8_t* payload = (uint8_t *)calloc(payload_size, sizeof(uint8_t));
+    validate_alloc(payload);
+
+    uint8_t *fill_pos = payload;
+    memcpy(fill_pos, msg->header, sizeof(*(msg->header)));
+
+    fill_pos += sizeof(msg->header);
+    memcpy(fill_pos, msg->question->qname, len_encoded_hostname);
+
+    fill_pos += len_encoded_hostname;
+    memcpy(fill_pos, &(msg->question->qtype), sizeof(uint16_t));
+
+    fill_pos += sizeof(uint16_t);
+    memcpy(fill_pos, &(msg->question->qclass), sizeof(uint16_t));
+
+    free(msg->question->qname);
+    msg->question->qname = NULL;
+
+    return payload;
+}
+
+void send_dns_msg(char *hostname) {
+    struct DNS_msg msg;
+    msg.header = (struct DNSHeader *)calloc(1, sizeof(struct DNSHeader));
+    validate_alloc(msg.header);
+
+    msg.question = (struct DNSQuestion *)calloc(1, sizeof(struct DNSQuestion));
+    validate_alloc(msg.question);
+
+    uint8_t *payload = pack_payload(&msg, hostname);
+
+    free(payload);
+    payload = NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -49,13 +95,7 @@ int main(int argc, char *argv[]) {
     }
 
     srand(time(NULL));
-    struct DNSHeader header = {0};
-    struct DNSQuestion question = {0};
+    send_dns_msg(argv[1]);
 
-    init_question(&question, argv[1]);
-    printf("hostname: %s\nencoded: %s\n", argv[1], question.qname);
-
-    free(question.qname);
-    question.qname = NULL;
     return 0;
 }
--- main.h
+++ main.h
@@ -15,25 +15,26 @@ DNSHeader.flags
 */
 struct DNSHeader {
     uint16_t id;
-    unsigned int qr: 1; // query (0) or response (1)?
-    unsigned int opcode: 4;
-    unsigned int aa: 1; // Authoritative Answer
-    unsigned int tc: 1; // Truncated - response was truncated
-    unsigned int rd: 1; // Recursion Desired
-    unsigned int ra: 1; // Recursion Available
-    unsigned int z: 3; // Not used, must be 0.
-    unsigned int rcode: 3; // Response Code
+    // unsigned int qr: 1; // query (0) or response (1)?
+    // unsigned int opcode: 4;
+    // unsigned int aa: 1; // Authoritative Answer
+    // unsigned int tc: 1; // Truncated - response was truncated
+    // unsigned int rd: 1; // Recursion Desired
+    // unsigned int ra: 1; // Recursion Available
+    // unsigned int z: 3; // Not used, must be 0.
+    // unsigned int rcode: 4; // Response Code
+    uint16_t flags;
     uint16_t qdcount; // num entries in the questions section
     uint16_t ancount; // num resource records in the answer section
     uint16_t nscount; // num name server resource records in the authority records section
     uint16_t arcount; // num resource records in the additional records section
-};
+} __attribute__((packed));
 
 struct DNSQuestion {
     uint8_t* qname;
     uint16_t qtype;
     uint16_t qclass;
-};
+} __attribute__((packed));
 
 struct DNSAnswer {
 
@@ -45,14 +46,15 @@ struct DNSAdditional {
 
 };
 
-
 struct DNS_msg {
-    struct DNSHeader header;
-    struct DNSQuestion question;
-};
+    struct DNSHeader *header;
+    struct DNSQuestion *question;
+} __attribute__((packed));
 
 void validate_alloc(void *mem_ptr);
 void init_header(struct DNSHeader *header);
-void init_question(struct DNSQuestion *question, char *hostname);
-char *encode_hostname(char *hostname);
+void init_question(struct DNSQuestion *question, char *hostname, size_t *len_encoded_hostname);
+void encode_hostname(char *hostname, char *en_hostname_out);
+uint8_t *pack_payload(struct DNS_msg *msg, char *hostname);
+void send_dns_msg(char *hostname);
 int main(int argc, char *argv[]);
